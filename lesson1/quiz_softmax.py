260a78306dc322689066956d28e08bbdc575b1c8
andrey <andreyfomin82@gmail.com>
2017-06-28 11:46:05 +0300

    quiz_softmax.ipynb

--- lesson1/quiz_softmax.py
+++ lesson1/quiz_softmax.py
@@ -1,8 +1,9 @@
 """Softmax."""
 
-scores = [3.0, 1.0, 0.2]
-
 import numpy as np
+import matplotlib.pyplot as plt
+
+scores = [3.0, 1.0, 0.2]
 
 
 # scores = np.array([[1, 2, 3, 6],
@@ -17,8 +18,6 @@ def softmax(x):
 print(softmax(scores))
 
 # Plot softmax curves
-import matplotlib.pyplot as plt
-
 x = np.arange(-2.0, 6.0, 0.1)
 scores = np.vstack([x, np.ones_like(x), 0.2 * np.ones_like(x)])
 
